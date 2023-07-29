f1afbd9d1bb782d817316625f424905b86655881
Daniel Chura <dchuramo@unsa.edu.pe>
2023-07-28 19:43:11 -0500

    actualizando modelos

--- virtualShop/app/models.py
+++ virtualShop/app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    gender_choices = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('U', 'Unisex'),
+    )
+    gender = models.CharField(max_length=1, choices=gender_choices)
+
+    def __str__(self):
+        return self.name
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through='CartItem')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.cart.user.username} - {self.product.name}"
--- virtualShop/app/views.py
+++ virtualShop/app/views.py
@@ -1,7 +1,20 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, Category
 
 # Create your views here.
 
 def home(request):
     return render(request, "app/home.html")
+
+def products_view(request):
+    # Obtener todos los productos de la base de datos
+    products = Product.objects.all()
+
+    # Obtener todas las categorías de la base de datos
+    categories = Category.objects.all()
+
+    context = {
+        'products': products,
+        'categories': categories,
+    }
+    return render(request, 'products.html', context)
