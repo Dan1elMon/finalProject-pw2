0fea72d491184045317a9dccb9003047593096da
Daniel Chura <dchuramo@unsa.edu.pe>
2023-07-31 17:56:35 -0500

    corrigiendo modelo

--- virtualShop/app/admin.py
+++ virtualShop/app/admin.py
@@ -1,6 +1,9 @@
 
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product
 
-admin.site.register(Product)
-admin.site.register(Category)
+##admin.site.register(Product)
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display =['id','title','discounted_price','category','product_image']
--- virtualShop/app/models.py
+++ virtualShop/app/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 CATEGORY_CHOICES=(
     ('TS', 'T-shirt'),
     ('SH', 'Shoes'),
-    ('GA', 'Gaps'),
+    ('CA', 'Caps'),
     ('JE', 'Jeans')
 
 )
@@ -15,23 +16,13 @@ class Category(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
-    selling_price=models.FloatField()
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
     description = models.TextField()
-    category = models.CharField(choices=CATEGORY_CHOICES,max_length=2)
-    product_image=models.ImageField(upload_to='product')
-    def _str_(self):
-        return self.title
-
-
-class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product, through='CartItem')
-    created_at = models.DateTimeField(auto_now_add=True)
-
-class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
+    composition = models.TextField(default='')
+    podapp = models.TextField(default='')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='product', blank=True, null=True)
 
     def __str__(self):
-        return f"{self.cart.user.username} - {self.product.name}"
+        return self.title
