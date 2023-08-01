c847ce1a1d47c43926b87836242bd1cac5898a24
Daniel Chura <dchuramo@unsa.edu.pe>
2023-08-01 13:49:37 -0500

    agregando login

--- virtualShop/app/urls.py
+++ virtualShop/app/urls.py
@@ -3,6 +3,9 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_view
+from .forms import LoginForm
+
 urlpatterns = [
     path('',views.home),
     path('about/', views.about,name="about"),
@@ -14,5 +17,5 @@ urlpatterns = [
 
     #log auth
     path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
-    
+    path('accounts/login/', auth_view.LoginView.as_view(template_name='app/login.html',authentication_form=LoginForm), name='login')
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
