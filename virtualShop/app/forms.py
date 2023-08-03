aacecc7fdd6b70b063a93316ffd9db95f05a6ec2
Daniel Chura <dchuramo@unsa.edu.pe>
2023-08-02 23:00:22 -0500

    creando reseteo de contrseña

--- virtualShop/app/forms.py
+++ virtualShop/app/forms.py
@@ -42,8 +42,8 @@ class MyPasswordChangeForm(PasswordChangeForm):
     new_password1 =forms.CharField(label='New Password', widget=forms.PasswordInput(attrs={'autocomplete':'current-password','class':'form-control'}))
     new_password2 =forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'autocomplete':'current-password','class':'form-control'}))
 
- 
-class MyPasswordResetForm(PasswordChangeForm):
+
+class MyPasswordResetForm(PasswordResetForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
 
 
